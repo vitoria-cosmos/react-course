6044c537b5545c6fd63934084bb52f5b1531b80b
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-07-12 20:20:04 -0300

    class 12

---
+++ aula12-loops/script.js
@@ -0,0 +1,20 @@
+
+// while = Enquanto
+
+x = 0;
+
+while (x < 10) {
+    document.write('</br> O valor de x é: ' + x);
+    x++;
+}
+
+x = 5;
+
+document.write('</br></br></br> O X está valendo: ' + x + '</br></br>');
+
+
+//  For = Para
+
+for (a = 0; a < x; a++) {
+    document.write('</br> O valor do A: ' + a);
+}
