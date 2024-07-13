c5e95ccba6def0b36b950e6428e6ec5cd2bc12d0
vitoria_cosmos <maria.v.cosmo@gmail.com>
2024-07-13 17:22:49 -0300

    class16

---
+++ aula16-webStorage/script.js
@@ -0,0 +1,12 @@
+// var nome = localStorage.nome;
+// console.log(nome);
+
+var nome = '';
+
+if (typeof localStorage.nome == 'undefined') {
+    localStorage.nome = prompt('Digite seu nome?');
+}
+
+nome = localStorage.nome;
+
+document.getElementById('nome').innerHTML = nome;
