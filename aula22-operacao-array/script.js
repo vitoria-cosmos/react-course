405c451ebebecab29afaa1efc1168f4aeb15230c
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-07-22 15:30:42 -0300

    class22

--- aula22-operacao-array/script.js
+++ aula22-operacao-array/script.js
@@ -1,4 +1,4 @@
-// exemplo 1
+// exemplo 1 - map
 // const lista = [1, 2, 3, 4, 5, 6];
 
 // const novaLista = lista.map(function(item) {
@@ -8,12 +8,33 @@
 // console.log(novaLista);
 
 
-// exemplo 2
+// exemplo 2 - index
+
+// const lista = [1, 2, 3, 4, 5, 6];
+
+// const novaLista = lista.map(function(item, index) {
+//     return item + index;
+// });
+
+// console.log(soma);
+
+// exemplo 3 - reduce
+
+// const lista = [1, 2, 3, 4, 5, 6];
+
+// const soma = lista.reduce(function(total, proximo) {
+//     return total + proximo;
+// });
+
+// console.log(soma);
+
+// exemplo 4 - find
 
 const lista = [1, 2, 3, 4, 5, 6];
 
-const novaLista = lista.map(function(item, index) {
-    return item + index;
+const find = lista.find(function(item) {
+    return item === 6;
 });
 
-console.log(soma);
+console.log(find);
+
