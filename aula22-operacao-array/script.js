6723c4e3475a0291646a6a93675fc6e8d4f123e1
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-07-22 15:06:03 -0300

    review

---
+++ aula22-operacao-array/script.js
@@ -0,0 +1,19 @@
+// exemplo 1
+// const lista = [1, 2, 3, 4, 5, 6];
+
+// const novaLista = lista.map(function(item) {
+//     return item * 5;
+// });
+
+// console.log(novaLista);
+
+
+// exemplo 2
+
+const lista = [1, 2, 3, 4, 5, 6];
+
+const novaLista = lista.map(function(item, index) {
+    return item + index;
+});
+
+console.log(soma);
