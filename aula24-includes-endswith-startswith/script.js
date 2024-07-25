fcb476183703c738be399075791fc74892e8d4a1
vitoria.sousa <maria.v.cosmo@gmail.com>
2024-07-25 09:49:48 -0300

    class24 and 25

---
+++ aula24-includes-endswith-startswith/script.js
@@ -0,0 +1,26 @@
+// includes
+
+// let nomes = ['Matheus', 'Lucas', 'Joao'];
+
+// console.log(nomes.includes('Lucas'));
+
+
+// if (nomes.includes('Matheus')) {
+//     console.log('MATHEUS ESTA NA LISTA');
+// } else {
+//     console.log('MATHEUS NÃO ESTA NA LISTA');
+// }
+
+
+// endWith
+let nome = 'Matheus';
+
+console.log(nome.endsWith('s'));
+
+
+// startsWith
+
+let myName = 'Vitoria';
+
+console.log(myName.startsWith('Vi'));
+
